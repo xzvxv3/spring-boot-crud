7cb2c210414c08ce3f015fe9f8f1fff32abeee38
xzvxv3 <xzvxv3@gmail.com>
2026-01-08 18:21:43 +0900

    Update, Delete 추가

--- src/main/java/com/example/xzvxv3/controller/BoardController.java
+++ src/main/java/com/example/xzvxv3/controller/BoardController.java
@@ -8,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -19,7 +22,7 @@ public class BoardController {
     @Autowired
     private PostRepository postRepository;
 
-    // 게시글 목록
+    // 게시글 목록, read
     @GetMapping("/posts")
     public String posts(Model model) {
         List<Post> list = postRepository.findAll();
@@ -27,11 +30,13 @@ public class BoardController {
         return "posts";
     }
 
+    // create
     @GetMapping("/posts/new")
     public String newPostForm() {
         return "create";
     }
 
+    // create
     @PostMapping("/posts/create")
     public String createPost(PostDto dto) {
         Post post = dto.toEntity(); // DTO -> Entity
@@ -43,4 +48,48 @@ public class BoardController {
         return "redirect:/posts";
     }
 
+    // read
+    @GetMapping("/posts/{id}")
+    public String read(@PathVariable Long id, Model model) {
+        Post post = postRepository.findById(id).orElse(null);
+
+        model.addAttribute("post", post);
+
+        return "read";
+    }
+
+    // update
+    @GetMapping("/posts/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        Post post = postRepository.findById(id).orElse(null);
+
+        model.addAttribute("post", post);
+
+        return "edit";
+    }
+
+    // update
+    @PostMapping("/posts/update")
+    public String update(PostDto dto) {
+        Post post = dto.toEntity();
+
+        if(postRepository.findById(post.getId()).orElse(null) != null) {
+            postRepository.save(post);
+        }
+
+        return "redirect:/posts/" + post.getId();
+    }
+
+    // Delete
+    @GetMapping("/posts/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        Post post = postRepository.findById(id).orElse(null);
+
+        if(post != null) {
+            postRepository.delete(post);
+            rttr.addFlashAttribute("msg", "삭제가 완료되었습니다.");
+        }
+
+        return "redirect:/posts";
+    }
 }
